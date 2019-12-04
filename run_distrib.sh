db6dc989336bd67fd4f47a4a0de9d92b9186b2a5
Jing Gu <jinggu@language.cs.ucdavis.edu>
2019-12-04 01:29:05 -0800

    init

--- run_distrib.sh
+++ run_distrib.sh
@@ -1,8 +1,8 @@
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export OMP_NUM_THREADS=4
 
 python -m torch.distributed.launch \
-    --nproc_per_node=1 main.py \
+    --nproc_per_node=8 main.py \
     --learning_rate 1e-4 \
     --gradient_accumulation_steps 4 \
     --num_train_epochs 20 \
